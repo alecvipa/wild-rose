5bb37ad20924d702064d13a31938c4eb8dccdc26
alecvipa <hebrit_626@hotmail.com>
2025-05-01 15:47:57 +0200

    added url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Redirect to https://xyncs.com
-const targetBaseUrl = 'https://www.vilpafilms.com/home';
+// const targetBaseUrl = 'https://www.vilpafilms.com/home';
 
 
 
@@ -33,8 +33,8 @@ const targetBaseUrl = 'https://www.vilpafilms.com/home';
 
 // Routes
 app.get('/', function (req, res) {
-    res.redirect(targetBaseUrl);
-    // res.render('home');
+    // res.redirect(targetBaseUrl);
+    res.render('home');
 
 });
 // app.get('/inicio', function (req, res) {
